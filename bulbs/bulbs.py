f2918191da6f6d96f36da492b43f7ed00454397f
Peter Hajas <peterhajas@gmail.com>
2015-11-16 20:22:20 -0800

    Implement a really basic way of using requests to get bulb names and toggle states

--- bulbs/bulbs.py
+++ bulbs/bulbs.py
@@ -1,4 +1,5 @@
 import flux_led
+import yaml
 
 class Bulb:
     def __init__(self, name, ip):
@@ -23,14 +24,32 @@ class Bulb:
         newOn = not self.isOn
         self.setOn(newOn)
 
+def bulbWithName(bulbs, name):
+    for bulb in bulbs:
+        if bulb.name == name:
+            return bulb
+    return None
+
 def allBulbs():
     seeker = flux_led.BulbSeeker()
     ips = seeker.discover(5)
 
+    # Reverse the name-to-ip map
+    names_to_ips_file = open('bulbs/bulbs.yaml', 'r')
+    names_to_ips = yaml.load(names_to_ips_file)
+    ips_to_names = { }
+
+    for key in names_to_ips.keys():
+        ip = names_to_ips[key]
+        ips_to_names[ip] = key
+
     bulbs = [ ]
 
     for ip in ips:
-        bulb = Bulb("Bulb", ip)
+        name = ips_to_names[ip]
+        bulb = Bulb(name, ip)
         bulbs.append(bulb)
+
+    names_to_ips_file.close()
 
     return bulbs
--- control.py
+++ control.py
@@ -1,3 +1,30 @@
-from flux_led import flux_led
+from bulbs import bulbs
+import cherrypy
 
+class ControlApp(object):
+    allBulbs = bulbs.allBulbs()
 
+    def index(self):
+        return "Hello, world!"
+    index.exposed = True
+
+    @cherrypy.expose
+    def off(self, name):
+        bulb = bulbs.bulbWithName(self.allBulbs, name)
+        bulb.turnOff()
+
+    @cherrypy.expose
+    def on(self, name):
+        bulb = bulbs.bulbWithName(self.allBulbs, name)
+        bulb.turnOn()
+
+    @cherrypy.expose
+    def allBulbNames(self):
+        bulb_names = ""
+        for bulb in self.allBulbs:
+            name = bulb.name
+            bulb_names = bulb_names + '\n' + name
+        bulb_names = bulb_names[1:]
+        return bulb_names
+
+cherrypy.quickstart(ControlApp())
